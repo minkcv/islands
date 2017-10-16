a94369e41db95e523d93ce28a7e4daabecacaa66
Will Smith <w.smith@mepcad.com>
2017-10-16 15:04:03 -0600

    rename file part 2

--- world.js
+++ world.js
@@ -1,14 +1,34 @@
-var material = new THREE.MeshPhongMaterial( { color: 0x40ef95, flatShading: true, overdraw: 0.5, shininess: 0 } );
+var greenMaterial = new THREE.MeshPhongMaterial( { color: 0x40ef95, flatShading: true, overdraw: 0.5, shininess: 0 } );
+var redMaterial = new THREE.MeshPhongMaterial( { color: 0xf76942, flatShading: true, overdraw: 0.5, shininess: 0 } );
+function generateWorld() {
+    var world = new THREE.Object3D();
+    var islandSpacing = 10;
+    var numIslands = 30;
+    for (var i = 0; i < numIslands; i++) {
+        var x = Math.round(islandSpacing - Math.random() * islandSpacing * 2) * 5 + 2.5;
+        var z = Math.round(islandSpacing - Math.random() * islandSpacing * 2) * 5 + 2.5;
+        world.add(generateIsland(x, z));
+    }
 
+    var light = new THREE.DirectionalLight( 0xffffff, 1.0 );
+    light.position.set(1, 3, 2).normalize();
+    world.add( light );
+    world.add( light.target );
+    
+    var grid = new THREE.GridHelper(500, 100, 0x000000, 0x000000);
+    world.add(grid);
+    return world;
+}
 
-function generateIsland(x, z)
-{
-    var width = Math.random() * 5 + 5;
-    var height = Math.random() * 3 + 2;
-    var depth = Math.random() * 5 + 5;
+function generateIsland(x, z) {
+    var width = Math.floor(Math.random() * 2) * 5 + 5;
+    //console.log(width);
+    var height = 1; //Math.floor(Math.random() * 3) + 2;
+    var depth = Math.floor(Math.random() * 2) * 5 + 5;
     var geometry = new THREE.BoxGeometry(width, height, depth);
-    var cube = new THREE.Mesh( geometry, material );
-    cube.position.x = Math.random() * 10 + x;
-    cube.position.z = Math.random() * 10 + z;
+    var cube = new THREE.Mesh( geometry, greenMaterial );
+    // Shift up to x and z with additional 2.5 if width ends in 5
+    cube.position.x = x + 2.5 * (Math.floor(width / 5) - 1);
+    cube.position.z = z + 2.5 * (Math.floor(depth / 5) - 1);
     return cube;
 }
