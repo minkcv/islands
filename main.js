ccec51f593cb85c7f0cf23e6110416928ee8e2a4
Will Smith <minkcv@gmail.com>
2017-10-15 21:58:41 -0600

    fix lighting

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ var height = threediv.clientHeight - 20;
 
 var scene = new THREE.Scene();
 scene.background = new THREE.Color(0x4286f4);
-var scale = 32;
+var scale = 16;
 var camera = new THREE.OrthographicCamera(width / -scale, width / scale, height / scale, height / -scale, 0, 4000);
 camera.position.z = 10;
-camera.position.y = 10;
+camera.position.y = 15;
 camera.rotation.x = -Math.PI / 4.0;
 var rotation = 0;
 var xPos = 0;
@@ -29,11 +29,10 @@ renderer.setSize(width, height);
 threediv.appendChild(renderer.domElement);
 
 var light = new THREE.DirectionalLight( 0xffffff, 1.0 );
-light.position.set(70, 70, 100).normalize();
+light.position.set(1, 3, 2).normalize();
 scene.add( light );
+scene.add( light.target );
 
-var axes = new THREE.AxisHelper(3, 3, 3);
-scene.add(axes);
 var grid = new THREE.GridHelper(50, 10, 0x000000, 0x000000);
 scene.add(grid);
 
@@ -46,27 +45,21 @@ scene.add( cube );
 function update() {
 	if (keys.d in keysDown) {
         scene.position.x += 0.2;
-        light.position.x += 0.2;
     }
     else if (keys.a in keysDown) {
         scene.position.x -= 0.2;
-        light.position.x -= 0.2;
     }
 	if (keys.s in keysDown) {
         scene.position.z += 0.2;
-        light.position.z += 0.2;
     }
     else if (keys.w in keysDown) {
         scene.position.z -= 0.2;
-        light.position.z -= 0.2;
     }
     if (keys.right in keysDown) {
 		scene.rotation.y -= 0.05;
-        light.rotation.y -= 0.05;
     }
     else if (keys.left in keysDown) {
 		scene.rotation.y += 0.05;
-        light.rotation.y += 0.05;
     }
 }
 
