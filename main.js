b321eb55138bfd6da32eef0bcae15de45707f57d
Will Smith <w.smith@mepcad.com>
2017-10-16 15:03:35 -0600

    generate islands better

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var height = threediv.clientHeight - 20;
 
 var scene = new THREE.Scene();
 scene.background = new THREE.Color(0x4286f4);
-//scene.rotation.y = -Math.PI / 4.0; // Start isometric
+
 var scale = 16;
 var camera = new THREE.OrthographicCamera(width / -scale, width / scale, height / scale, height / -scale, 0, 4000);
 camera.translateZ(2000); // Move back so camera is centered on 0, 0, 0
@@ -25,29 +25,20 @@ var cam = new THREE.Object3D(); // Parent for camera to rotate/pan easier
 cam.add(camera);
 scene.add(cam);
 
-// Setup isometri view
+var axes = new THREE.AxisHelper(3);
+cam.add(axes);
+
+// Setup isometric view
 cam.rotateY(Math.PI / 4.0);
 cam.rotateX(-Math.PI / 4.0);
+axes.rotateX(Math.PI / 4.0);
+axes.rotateY(-Math.PI / 4.0);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 threediv.appendChild(renderer.domElement);
 
-var world = new THREE.Object3D();
-var islandSpread = 200;
-var numIslands = 30;
-for (var i = 0; i < numIslands; i++)
-    world.add(generateIsland((0.5 - Math.random()) * islandSpread, (0.5 - Math.random()) * islandSpread));
-
-
-var light = new THREE.DirectionalLight( 0xffffff, 1.0 );
-light.position.set(1, 3, 2).normalize();
-world.add( light );
-world.add( light.target );
-
-var grid = new THREE.GridHelper(500, 100, 0x000000, 0x000000);
-world.add(grid);
-
+var world = generateWorld();
 scene.add(world);
 
 var rotateSpeed = 0.05;
@@ -80,11 +71,13 @@ function update() {
         cam.rotateX(Math.PI / 4.0);
         cam.rotateY(rotateSpeed);
         cam.rotateX(-Math.PI / 4.0);
+        axes.rotateY(-rotateSpeed);
     }
     else if (keys.left in keysDown) {
         cam.rotateX(Math.PI / 4.0);
         cam.rotateY(-rotateSpeed);
         cam.rotateX(-Math.PI / 4.0);
+        axes.rotateY(rotateSpeed);
     }
 }
 
